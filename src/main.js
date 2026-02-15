bf91f8eaa4c1a7628223647fc45e5cdfa022566f
varshaannbijuu <varshaannb@gmail.com>
2026-02-15 11:11:42 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-// src/main.js
-import { mount } from 'svelte';
-import App from './App.svelte';
-import './app.css';
+import { mount } from 'svelte'
+import './app.css'
+import App from './App.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app'),
-});
+})
 
-export default app;
+export default app
